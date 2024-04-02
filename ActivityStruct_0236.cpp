d96ca202ae0232aa2bb4fefb06ef8327e6d8ffba
akmaalsyh <taufansyah50@gmail.com>
2024-04-02 18:18:55 +0700

    memanggil cout mhs nim

--- ActivityStruct_0236.cpp
+++ ActivityStruct_0236.cpp
@@ -37,6 +37,9 @@ int main ()
         cin.ignore(1, '\n');
         cout << endl;
     }
-    
+
     for (int i=0;i<3;i++) 
     {
+        cout << endl;
+        cout << "Data Mahasiswa ke-" << (i+1) << ":" << endl;
+        cout << "\n NIM : " << mhs[i].nim;
