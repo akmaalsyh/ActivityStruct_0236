86c314c39cfc03d699835067be50695467b9041c
akmaalsyh <taufansyah50@gmail.com>
2024-04-02 18:17:18 +0700

    membuat cout alamat kota

--- ActivityStruct_0236.cpp
+++ ActivityStruct_0236.cpp
@@ -25,7 +25,9 @@ int main ()
         getline (cin, mhs[i].nim);
         cout << "Nama Mahasiswa : ";
         getline (cin, mhs[i].nama);
-        
+
         cout << "Alamat mahasiswa : " << endl;
         cout << "\t Nama Desa : ";
         cin >> mhs[i].alamat.desa;
+        cout << "\t Nama Kota : ";
+        cin >> mhs[i].alamat.kota;
